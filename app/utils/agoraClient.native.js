3561fb39c6dab3a4e74e33187c12abb010893f9c
muditam <madhur.muditam@gmail.com>
2026-05-29 16:38:27 +0530

    agora client removed

--- app/utils/agoraClient.native.js
+++
@@ -1,3 +0,0 @@
-export function getAgoraModule() {
-  return require("react-native-agora");
-}
--- app/utils/agoraClient.web.js
+++
@@ -1,3 +0,0 @@
-export function getAgoraModule() {
-  throw new Error("Agora native calling is only available in the mobile app.");
-}
